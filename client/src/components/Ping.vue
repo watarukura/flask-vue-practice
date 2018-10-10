5b1c9381d5d8eeb96983056c1955899d6936d0d6
wataru-kurashima <wataru-kurashima@hands-lab.com>
2018-10-10 13:30:43 +0900

    add prettier

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -1,16 +1,37 @@
-<template>
-    <div>
-        <p>{{ msg }}</p>
-    </div>
-</template>
-
 <script>
+// eslint-disable-next-line
+import axios from 'axios';
+
 export default {
-    name: 'Ping',
-    data() {
-        return {
-            msg: 'Hello!',
-        };
+  // eslint-disable-next-line
+  name: 'Ping',
+  data() {
+    return {
+      // eslint-disable-next-line
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      // eslint-disable-next-line
+      const path = 'http://localhost:5000/ping';
+      axios
+        .get(path)
+        // eslint-disable-next-line
+        .then(res => {
+          this.msg = res.data;
+        })
+        // eslint-disable-next-line
+        .catch(error => {
+          // eslint-disable-next-line
+          console.errro(error);
+        });
+      // eslint-disable-next-line
     },
+  },
+  created() {
+    this.getMessage();
+    // eslint-disable-next-line
+  },
 };
 </script>
